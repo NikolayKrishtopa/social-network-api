d048ab3a32647dc8cb686089fb71ff5b0442ad34
Nikolay Krishtopa <nikolay.krishtopa@gmail.com>
2022-12-24 02:21:45 +0400

    debug

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,7 @@ const UnauthorizedError = require("../utils/errors/UnauthorizedError");
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
   if (!token) {
-    throw new UnauthorizedError(`нет куки ${TOKEN_ENCRYPT_KEY}`);
+    throw new UnauthorizedError(`нет куки ${req.cookies}`);
   }
   let payload;
   try {
