44cc6158b1e1bb01efc34542ddab99ac0d16d03c
Nikolay Krishtopa <nikolay.krishtopa@gmail.com>
2022-12-23 20:54:56 +0400

    hotfix/fix body validator for login case

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,22 @@
-const router = require('express').Router();
-const userBodyValidator = require('../middlewares/requestValidators/userBodyValidator');
-const routerMovies = require('./movies');
-const routerUsers = require('./users');
-const { login, logout } = require('../controllers/login');
-const { createUser } = require('../controllers/users');
-const auth = require('../middlewares/auth');
-const NotFoundError = require('../utils/errors/NotFoundError');
-const ERRORS_MESSAGES = require('../utils/ERRORS_MESSAGES');
+const router = require("express").Router();
+const userBodyValidator = require("../middlewares/requestValidators/userBodyValidator");
+const routerMovies = require("./movies");
+const routerUsers = require("./users");
+const { login, logout } = require("../controllers/login");
+const { createUser } = require("../controllers/users");
+const auth = require("../middlewares/auth");
+const NotFoundError = require("../utils/errors/NotFoundError");
+const ERRORS_MESSAGES = require("../utils/ERRORS_MESSAGES");
+const userBodyValidatorForLogin = require("../middlewares/requestValidators/userBodyValidatorForLogin");
 
-router.post('/signin', userBodyValidator, login);
-router.delete('/signout', logout);
-router.post('/signup', userBodyValidator, createUser);
+router.post("/signin", userBodyValidatorForLogin, login);
+router.delete("/signout", logout);
+router.post("/signup", userBodyValidator, createUser);
 
 router.use(auth);
-router.use('/movies', routerMovies);
-router.use('/users', routerUsers);
-router.use('*', () => {
+router.use("/movies", routerMovies);
+router.use("/users", routerUsers);
+router.use("*", () => {
   throw new NotFoundError(ERRORS_MESSAGES.NOT_FOUND);
 });
 
