0015f8561e4e5eb34e24076e7670ff15658a2d55
Nikolay Krishtopa <nikolay.krishtopa@gmail.com>
2023-05-08 01:24:36 +0400

    fix: resolve all the linter errors

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,7 +1,11 @@
-const ERRORS_MESSAGES = require("../utils/ERRORS_MESSAGES");
+const ERRORS_MESSAGES = require('../utils/ERRORS_MESSAGES');
 
 module.exports = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
-  res.status(statusCode).send(statusCode !== 500 ? { message } : { message: ERRORS_MESSAGES.DEFAULT_ERROR});
+  res.status(statusCode)
+    .send(
+      statusCode !== 500 ? { message }
+        : { message: ERRORS_MESSAGES.DEFAULT_ERROR },
+    );
   next();
 };
