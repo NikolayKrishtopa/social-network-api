f4c178a9cddbd44b2173bb84c3bfdd9fafbd6e01
Nikolay Krishtopa <nikolay.krishtopa@gmail.com>
2023-05-10 14:41:12 +0400

    fix: add mesto to the whitelist

--- middlewares/verifyOrigin.js
+++ middlewares/verifyOrigin.js
@@ -1,6 +1,8 @@
 const allowedCors = [
   'http://movies.nikolaykrishtopa.nomoredomains.club',
   'https://movies.nikolaykrishtopa.nomoredomains.club',
+  'https://mesto.nikolaykrishtopa.nomoredomains.icu/',
+  'http://mesto.nikolaykrishtopa.nomoredomains.icu/',
   'http://localhost:3000',
   'http://localhost:3001',
   'http://localhost:8080',
